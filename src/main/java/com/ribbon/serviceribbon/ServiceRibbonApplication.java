6e6caebbd9ad8025afd56d330823bb7c6e0833fe
fjl <fengjianglin6817@dingtalk.com>
2018-06-23 14:49:53 +0800

    Initial commit

---
+++ src/main/java/com/ribbon/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,41 @@
+package com.ribbon.serviceribbon;
+
+import com.netflix.loadbalancer.IRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Administrator
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableHystrix//断路器
+@EnableHystrixDashboard
+/**
+ * 对指定的服务指定负载规则
+ */
+@RibbonClient(name = "service-hi",configuration = IRule.class)
+/**
+ *
+ * 启用服务熔断机制
+ */
+@EnableCircuitBreaker
+public class ServiceRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRibbonApplication.class, args);
+	}
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
